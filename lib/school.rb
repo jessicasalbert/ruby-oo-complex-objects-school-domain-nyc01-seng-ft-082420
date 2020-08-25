6ea22e9a7808b5c3e1e370375b92e9def878dd10
jessicasalbert <jessicarsalbert@gmail.com>
2020-08-25 18:51:49 -0400

    completed lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+require 'pry'
+class School
+
+    attr_accessor :name
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if roster[grade] ||= []
+        end
+        roster[grade] << student
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+      roster.each do |grade, students|
+          roster[grade] = students.sort
+      end
+      roster
+    end 
+
+end
